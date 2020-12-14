e79ce9139a0338f803da39bef6e2140dd88b4669
dfirsec <12067255+dfirsec@users.noreply.github.com>
2020-12-14 11:53:55 -0500

    Updated

--- mac_lookup.py
+++ mac_lookup.py
@@ -11,24 +11,27 @@ import requests
 from tqdm import tqdm
 
 __author__ = "DFIRSec (@pulsecode)"
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 __description__ = "Simple script to query for MAC address vendor info"
 
-BASE_DIR = Path(__file__).resolve().parent
-MACLIST = BASE_DIR.joinpath('macaddress.io-db.json')
-REGEX = re.compile(r'(([a-fA-F0-9](:|-)?){6,17})')
-MACVEND = 'https://macvendors.co/api/'
-MACDB = 'https://macaddress.io/database/macaddress.io-db.json'
+parent = Path(__file__).resolve().parent
+maclist = parent.joinpath("macaddress.io-db.json")
+regex = re.compile(r"(([a-fA-F0-9](:|-)?){6,17})")
+macvend = "https://macvendors.co/api/"
+macdb = "https://macaddress.io/database/macaddress.io-db.json"
 
 
 def connect(url):
     try:
-        resp = requests.get(url, timeout=5)
+        ua = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/43.0"}
+        resp = requests.get(url, headers=ua)
         return resp
-    except (requests.exceptions.Timeout,
-            requests.exceptions.HTTPError,
-            requests.exceptions.ConnectionError,
-            requests.exceptions.RequestException):
+    except (
+        requests.exceptions.Timeout,
+        requests.exceptions.HTTPError,
+        requests.exceptions.ConnectionError,
+        requests.exceptions.RequestException,
+    ):
         pass
     except Exception:
         print("\033[31m[x]\033[0m Connection error encountered")
@@ -39,35 +42,39 @@ def download_db(path, url):
         resp = requests.get(url, stream=True)
         if resp.status_code == 200:
             f_size = int(resp.headers.get("Content-Length", 0))
-            pbar = tqdm(iterable=resp.iter_content(chunk_size=1024),
-                        desc='[+] \u001b[35mDownloading\033[0m',
-                        total=f_size,
-                        unit="B",
-                        unit_scale=True,
-                        unit_divisor=1024)
-            with open(path, 'wb') as db_file:
+            pbar = tqdm(
+                iterable=resp.iter_content(chunk_size=1024),
+                desc="[+] \u001b[35mDownloading\033[0m",
+                total=f_size,
+                unit="B",
+                unit_scale=True,
+                unit_divisor=1024,
+            )
+            with open(path, "wb") as db_file:
                 for data in pbar:
                     db_file.write(data)
                     pbar.update(len(data))
-    except (requests.exceptions.Timeout,
-            requests.exceptions.HTTPError,
-            requests.exceptions.ConnectionError,
-            requests.exceptions.RequestException):
+    except (
+        requests.exceptions.Timeout,
+        requests.exceptions.HTTPError,
+        requests.exceptions.ConnectionError,
+        requests.exceptions.RequestException,
+    ):
         print("\033[31m[x]\033[0m Download error encountered")
 
 
 def mac_vend(query):
-    resp = connect(MACVEND + query)
-    return json.dumps(resp.json()['result'], sort_keys=True, indent=4)
+    resp = connect(macvend + query)
+    return json.dumps(resp.json()["result"], sort_keys=True, indent=4)
 
 
 def mac_list(mac_addr):
-    mac_vendor = [json.loads(line) for line in open(MACLIST, 'r', encoding='utf-8')]  # nopep8
+    mac_vendor = [json.loads(line) for line in open(maclist, "r", encoding="utf-8")]
     try:
         count = 0
         for mac in mac_vendor:
-            match = re.search(REGEX, mac_addr)
-            if mac['oui'] == match.group(0).upper():
+            match = re.search(regex, mac_addr)
+            if mac["oui"] in match.group(0).upper():
                 print(f"{'MAC Addr':12}: \033[1;36;40m{match.group(0)}\033[0m")
                 print(f"{'Company':12}: {mac['companyName']}")
                 print(f"{'Mac Prefix':12}: {mac['oui']}")
@@ -89,21 +96,20 @@ def modified_date(db_file):
 
 
 def main(mac_addr, mac_file, update):
-    if not MACLIST.exists():
-        print("[-]\033[33m Local MAC DB is missing, attempting to download...\033[0m")  # nopep8
-        download_db(MACLIST, MACDB)
+    if not maclist.exists():
+        print("[-]\033[33m Local MAC DB is missing, attempting to download...\033[0m")
+        download_db(maclist, macdb)
 
     if mac_addr:
         try:
-            if not any(char in mac_addr for char in ['-', ':']):
-                mac_addr = ':'.join(mac_addr[i:i+2]
-                                    for i in range(0, len(mac_addr), 2))
-            mac_addr = mac_addr.replace('-', ':')
+            if not any(char in mac_addr for char in ["-", ":"]):
+                mac_addr = ":".join(mac_addr[i : i + 2] for i in range(0, len(mac_addr), 2))
+            mac_addr = mac_addr.replace("-", ":")
             print("\n\033[1;32;40m[ Querying macvendors database ]\033[0m")
             results = json.loads(mac_vend(mac_addr))
 
             print("\033[1;30;40m.\033[0m" * 32)
-            if 'error' not in results:
+            if "error" not in results:
                 print(f"\033[1;36;40m{'MAC Addr':12}: {mac_addr}\033[0m")
                 for k, v in results.items():
                     print(f"{k.title().replace('_', ' '):12}: {v}")
@@ -125,32 +131,30 @@ def main(mac_addr, mac_file, update):
             with open(mac_file) as f:
                 text = [text.strip() for text in f.readlines()]
                 for mac_addr in text:
-                    if not any(char in mac_addr for char in ['-', ':']):
-                        mac_addr = ':'.join(mac_addr[i:i+2]
-                                            for i in range(0, len(mac_addr), 2))
+                    if not any(char in mac_addr for char in ["-", ":"]):
+                        mac_addr = ":".join(mac_addr[i : i + 2] for i in range(0, len(mac_addr), 2))
 
-                    mac_addr = mac_addr.replace('-', ':')
+                    mac_addr = mac_addr.replace("-", ":")
                     print("\033[1;30;40m.\033[0m" * 32)
 
                     try:
                         results = json.loads(mac_vend(mac_addr))
-                        if 'error' not in results:
-                            print(
-                                f"\033[1;36;40m{'MAC Addr':12}: {mac_addr}\033[0m")
+                        if "error" not in results:
+                            print(f"\033[1;36;40m{'MAC Addr':12}: {mac_addr}\033[0m")
                             for k, v in results.items():
                                 print(f"{k.title().replace('_', ' '):12}: {v}")
                         else:
-                            print(f"[-]\033[33m No results for {mac_addr}\033[0m")  # nopep8
+                            print(f"[-]\033[33m No results for {mac_addr}\033[0m")
                     except Exception:
                         # defaults to local db if query fails
                         mac_list(mac_addr)
 
     if update:
         try:
-            print(f"[+] Last updated: {modified_date(MACLIST)}")
-            input("[?]\033[33m Press Enter to continue, or Ctrl-C to cancel...\033[0m")  # nopep8
-            print("[+]\033[32m Updating database...\033[0m")  # nopep8
-            download_db(MACLIST, MACDB)
+            print(f"[+] Last updated: {modified_date(maclist)}")
+            input("[?]\033[33m Press Enter to continue, or Ctrl-C to cancel...\033[0m")
+            print("[+]\033[32m Updating database...\033[0m")
+            download_db(maclist, macdb)
         except KeyboardInterrupt:
             print("\n[-]\033[33m Update canceled\033[0m")
 
@@ -171,15 +175,12 @@ if __name__ == "__main__":
     print(f"\033[36m{banner}\033[0m")
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', dest='mac', metavar="MAC ADDRESS",
-                        help="single mac address")
-    parser.add_argument('-f', dest='file', metavar="FILE",
-                        help="file with mac addresses")
-    parser.add_argument('-u', dest='update', action='store_true',
-                        help="update local database")
+    parser.add_argument("-m", dest="mac", metavar="MAC ADDRESS", help="single mac address")
+    parser.add_argument("-f", dest="file", metavar="FILE", help="file with mac addresses")
+    parser.add_argument("-u", dest="update", action="store_true", help="update local database")
     args = parser.parse_args()
 
     if not (args.mac or args.file or args.update):
-        parser.error("\033[33m No action requested: Include mac address (-m) or file (-f) or update (-u)\033[0m")  # nopep8
+        parser.error("\033[33m No action requested: Include mac address (-m) or file (-f) or update (-u)\033[0m")
 
     main(mac_addr=args.mac, mac_file=args.file, update=args.update)
